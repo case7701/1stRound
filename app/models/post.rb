ca52c0eece5689f7c19fb5fe4b84612e6540985c
Casey Sullivan <case7701@hotmail.com>
2011-07-02 18:02:06 -0500

    blog

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+	validates :body, :presence => true
+	validates :title, :presence => true,
+		:uniqueness => true,
+		:length => { :within => 1..20 },
+		:exclusion => { :in => ["Title", "Post"] }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '155cdd11cb0fb71d44eadd36a6d2ac23e558652813a6f7e40c3591793afc53a9d0ac2d8f7ee89f420707998e7544fcb0afd917abc6a35091eb2e586d458b6533'
